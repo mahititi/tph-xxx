d3ad63b2d0e395a2bc38da2cb481446108ac3992
mahititi <43207594+mahititi@users.noreply.github.com>
2018-09-25 18:58:01 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+window.onload = setTimeout(popup, 10000);
+
+function popup(){
+	if (confirm ("Hey le site xxxvideosxxx est super mega trop cool, viens dessus stp please")) {
+		document.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+	}
+}
